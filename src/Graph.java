39290594c3dd9239a846cbcbce884998f3fe0823
Sifan Ye, on his MBP <ysf199711@126.com>
2019-02-10 23:25:41 -0500

    Cleanup imports

--- src/Graph.java
+++ src/Graph.java
@@ -1,13 +1,9 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Polygon;
 import java.awt.image.BufferedImage;
-
 import java.io.File;
 import java.io.IOException;
-
 import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
